15fb52d2844b3aa0d6bc88db55c18ef9db855df8
tkasakov <tihomirkasakov@gmail.com>
2017-08-10 16:23:39 +0300

    Added some files

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Collections;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public IEnumerable ApplicationUsers { get; internal set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
